0ad177634f0ef3afb6eb739b291b721b34949877
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-14 14:26:22 -0600

    Added query 2 to laura_hofer.sql

--- laura_hofer.sql
+++ laura_hofer.sql
@@ -1,8 +1,17 @@
 
---uses the database classicmodels
+-- uses the database classicmodels
 use classicmodels;
 
+-- query one
 select productName as `Name`, productLine as `Product Line`, buyPrice as `Buy Price` 
 from products
 order by buyPrice desc
 ; 
+
+-- query two
+select contactFirstName as `First Name`, contactLastName as `Last Name`, city as `City`
+from customers 
+where country='Germany'
+;
+
+
