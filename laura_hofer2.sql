cf29f1936f22d4f294892d0915982ec2e67afd0c
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-14 16:43:13 -0600

    Added query 2 to laura_hofer2.sql

--- laura_hofer2.sql
+++ laura_hofer2.sql
@@ -8,3 +8,11 @@ join employees as e on c.salesRepEmployeeNumber = e.employeeNumber
 order by c.customerName
 ;
 
+-- query two
+select p.productName as `Product Name`, od.quantityOrdered as `Total # Ordered`, od.quantityOrdered*od.priceEach as `Total Sale`
+from orderdetails as od
+join products as p on od.productCode = p.productCode
+group by `Product Name`
+order by `Total Sale` desc
+;
+
