aabd16f9d2e1328359da9719cf2757013ed5d08f
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-15 15:37:55 -0600

    Added query 1 to banking.sql

---
+++ banking.sql
@@ -0,0 +1,8 @@
+
+use banking;
+
+-- query 1 
+--  For each product, show the product name "Product" and the product type name "Type".
+select NAME as `Product`, PRODUCT_TYPE_CD as `Type`
+from product
+;
