ab3b42fee93bb9bcb1e8350815fd146dd2003c3c
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-15 16:44:15 -0600

    Added query 5 to banking.sql

--- banking.sql
+++ banking.sql
@@ -24,9 +24,26 @@ from employee
 
 -- query four
 -- Show the last name and title of each employee, along with the last name and title of that employee's boss.
-select * from employee;
 
 select e.LAST_NAME as `Last Name`, e.TITLE as `Title`, e2.LAST_NAME as `Supervisor`, e2.TITLE as `Supervisor Title`
 from employee as e
 join employee as e2 on e.SUPERIOR_EMP_ID = e2.EMP_ID 
+;
+
+-- query five
+-- For each account, show the name of the account's product, the available balance, and the customer's last name.
+select * from account;
+
+-- by individual
+select a.ACCOUNT_ID as `Account`, a.PRODUCT_CD as `Product`, a.AVAIL_BALANCE as `Available Balance`, i.LAST_NAME as `Last Name` 
+from account as a
+left join customer as c on a.CUST_ID = a.CUST_ID 
+join individual as i on c.CUST_ID = i.CUST_ID 
+;
+
+-- by officer
+select a.ACCOUNT_ID as `Account`, a.PRODUCT_CD as `Product`, a.AVAIL_BALANCE as `Available Balance`, o.LAST_NAME as `Last Name` 
+from account as a
+left join customer as c on a.CUST_ID = a.CUST_ID 
+join officer as o on c.CUST_ID = o.CUST_ID 
 ;
