76000b3d0fe601aa96cb6b76e913fae3f5a7e8a8
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-15 14:01:50 -0600

    Added query 4 to laura_hofer2.sql

--- laura_hofer2.sql
+++ laura_hofer2.sql
@@ -16,3 +16,26 @@ group by `Product Name`
 order by `Total Sale` desc
 ;
 
+
+-- query three
+-- Write a query which lists order status and the # of orders with that status. 
+-- Column headers should be “Order Status” and “# Orders”. Sort alphabetically by status.
+select status as `Order Status`, COUNT(status) as `# Orders`
+from orders as o
+group by status 
+;
+
+-- query four
+-- Write a query to list, for each product line, the total # of products sold from that product line. 
+-- The first column should be “Product Line” and the second should be “# Sold”. Order by the second column descending.
+select pl.productLine as `Product Line`, SUM(od.quantityOrdered) as `# Sold`
+from productlines as pl
+join products as p on p.productLine = pl.productLine 
+join orderdetails as od on p.productCode = od.productCode 
+group by pl.productLine 
+order by `# Sold` desc
+;
+
+
+
+
