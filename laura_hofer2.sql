d9d116186d42c7d3574597816f33b43ad1e506e0
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-15 15:25:02 -0600

    Added query 6 to laura_hofer2.sql

--- laura_hofer2.sql
+++ laura_hofer2.sql
@@ -52,6 +52,14 @@ group by `Sales Rep`
 order by `Total Sales` desc
 ;
 
+-- query 6
+-- Your product team is requesting data to help them create a bar-chart of monthly sales since the company’s inception. 
+-- Write a query to output the month (January, February, etc.), 4-digit year, and total sales for that month. 
+-- The first column should be labeled ‘Month’, the second ‘Year’, and the third should be ‘Payments Received’. 
+-- Values in the third column should be formatted as numbers with two decimals – for example: 694,292.68.
 
-
+select DATE_FORMAT(paymentDate, "%M") as `Month`, DATE_FORMAT(paymentDate, "%Y") as `Year`, FORMAT(SUM(amount), 2) as `Payments Received`  
+from payments 
+group by `Year`, `Month`
+;
 
