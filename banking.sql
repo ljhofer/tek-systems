feb4c49c760bf38ce38adc95590f00267faa25d2
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-15 15:54:26 -0600

    Added query 2 to banking.sql

--- banking.sql
+++ banking.sql
@@ -6,3 +6,14 @@ use banking;
 select NAME as `Product`, PRODUCT_TYPE_CD as `Type`
 from product
 ;
+
+
+-- query two 
+-- For each branch, list the branch name and city, plus the last name and title of each employee who works in that branch.
+select * from branch;
+select * from employee;
+
+select b.name as `Branch Name`, b.city as `City`, e.LAST_NAME as `Last Name`, e.TITLE as `Title`
+from branch as b
+join employee as e on b.BRANCH_ID = e.ASSIGNED_BRANCH_ID 
+;
